499a01908e115700e2839810a77d9c070d0c41a7
Erik Schnetter <schnetter@gmail.com>
2013-11-28 11:52:56 -0500

    Output message at shutdown

--- rpc_main.cc
+++ rpc_main.cc
@@ -4,6 +4,7 @@
 #include "rpc_server_mpi.hh"
 
 #include <cassert>
+#include <iostream>
 
 int rpc_main(int argc, char** argv);
 
@@ -11,6 +12,13 @@ int main(int argc, char** argv)
 {
   rpc::server = new rpc::server_mpi(argc, argv);
   int iret = rpc::server->event_loop(rpc_main);
+  if (rpc::server->rank() == 0) {
+    if (iret == 0) {
+      std::cout << "Done: success.\n";
+    } else {
+      std::cout << "Done: failure (error=" << iret << ").\n";
+    }
+  }
   delete rpc::server;
   return iret;
 }

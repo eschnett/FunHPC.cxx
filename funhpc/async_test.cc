1b4ac537a044babc9f0dcbe15a786ac3d5e5341c
Erik Schnetter <schnetter@gmail.com>
2015-02-10 22:41:52 -0500

    More remote async functionality

--- funhpc/async_test.cc
+++ funhpc/async_test.cc
@@ -27,3 +27,49 @@ TEST(funhpc, async_remote2) {
   auto fres = async(rlaunch::async, 1 % size(), add, 1, 2);
   EXPECT_EQ(3, fres.get());
 }
+
+void delay() {
+  qthread::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
+int idelay() {
+  qthread::this_thread::sleep_for(std::chrono::milliseconds(100));
+  return 1;
+}
+
+TEST(funhpc, async_async) {
+  auto fres = async(rlaunch::async, 1 % size(), delay);
+  EXPECT_FALSE(fres.is_ready());
+  fres.wait();
+  auto ires = async(rlaunch::async, 1 % size(), idelay);
+  EXPECT_FALSE(ires.is_ready());
+  EXPECT_EQ(1, ires.get());
+}
+
+TEST(funhpc, async_sync) {
+  auto fres = async(rlaunch::sync, 1 % size(), delay);
+  EXPECT_TRUE(fres.is_ready());
+  auto ires = async(rlaunch::sync, 1 % size(), idelay);
+  EXPECT_TRUE(fres.is_ready());
+  EXPECT_EQ(1, ires.get());
+}
+
+TEST(funhpc, async_deferred) {
+  auto fres = async(rlaunch::deferred, 1 % size(), delay);
+  EXPECT_FALSE(fres.is_ready());
+  qthread::this_thread::sleep_for(std::chrono::milliseconds(200));
+  EXPECT_FALSE(fres.is_ready());
+  fres.wait();
+  auto ires = async(rlaunch::deferred, 1 % size(), idelay);
+  EXPECT_FALSE(ires.is_ready());
+  qthread::this_thread::sleep_for(std::chrono::milliseconds(200));
+  EXPECT_FALSE(ires.is_ready());
+  EXPECT_EQ(1, ires.get());
+}
+
+TEST(funhpc, async_detached) {
+  auto fres = async(rlaunch::detached, 1 % size(), delay);
+  EXPECT_FALSE(fres.valid());
+  auto ires = async(rlaunch::detached, 1 % size(), idelay);
+  EXPECT_FALSE(ires.valid());
+  qthread::this_thread::sleep_for(std::chrono::milliseconds(200));
+}

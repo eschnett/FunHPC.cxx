fe7b7b56559ca38f3b7c16eb1faa4cee33de96cd
Erik Schnetter <schnetter@gmail.com>
2015-02-20 15:22:18 -0500

    Improve future serialization

--- funhpc/serialize_shared_future.hpp
+++ funhpc/serialize_shared_future.hpp
@@ -2,21 +2,58 @@
 #ifndef SERIALIZE_SHARED_FUTURE_HPP
 #define SERIALIZE_SHARED_FUTURE_HPP
 
+#include <funhpc/async.hpp>
+#include <funhpc/rptr.hpp>
 #include <qthread/future.hpp>
 
 #include <cereal/access.hpp>
 #include <cereal/types/memory.hpp>
 
 namespace qthread {
+
+namespace detail {
+template <typename T>
+T shared_future_get(const funhpc::rptr<shared_future<T>> &pf) {
+  T val = pf->get();
+  delete pf.get_ptr();
+  return val;
+}
+}
+
 template <typename Archive, typename T>
 void save(Archive &ar, const shared_future<T> &f) {
-  ar(f.get());
+  bool v = f.valid();
+  ar(v);
+  if (v) {
+    bool r = f.ready();
+    ar(r);
+    if (r) {
+      ar(f.get());
+    } else {
+      funhpc::rptr<shared_future<T>> pf(new shared_future<T>(f));
+      ar(pf);
+    }
+  }
 }
+
 template <typename Archive, typename T>
 void load(Archive &ar, shared_future<T> &f) {
-  T val;
-  ar(val);
-  f = make_ready_future(std::move(val));
+  bool v;
+  ar(v);
+  if (v) {
+    bool r;
+    ar(r);
+    if (r) {
+      T val;
+      ar(val);
+      f = make_ready_future(std::move(val));
+    } else {
+      funhpc::rptr<shared_future<T>> pf;
+      ar(pf);
+      f = funhpc::async(funhpc::rlaunch::async, pf.get_proc(),
+                        detail::shared_future_get<T>, pf);
+    }
+  }
 }
 }
 

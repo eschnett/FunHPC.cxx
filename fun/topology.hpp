55690bd21ab49378b33ea08779c7e0c9e0983d0b
Erik Schnetter <schnetter@gmail.com>
2015-03-03 13:22:37 -0500

    Correct topology implementation -- can't store references

--- fun/topology.hpp
+++ fun/topology.hpp
@@ -1,7 +1,8 @@
 #ifndef FUN_TOPOLOGY_HPP
 #define FUN_TOPOLOGY_HPP
 
-#include <tuple>
+#include <array>
+#include <utility>
 
 namespace fun {
 
@@ -9,10 +10,15 @@ namespace fun {
 
 template <typename T> struct connectivity {
   typedef T value_type;
-  std::tuple<const T &, const T &> data;
-  connectivity(const T &x0, const T &x1)
-      : data(std::forward_as_tuple(x0, x1)) {}
-  template <std::ptrdiff_t I> const T &get() const {
+  // std::tuple<const T &, const T &> data;
+  // std::tuple<T, T> data;
+  // connectivity(const T &x0, const T &x1)
+  //     : data(std::forward_as_tuple(x0, x1)) {}
+  std::array<T, 2> data;
+  template <typename T0, typename T1>
+  connectivity(T0 &&x0, T1 &&x1)
+      : data{{std::forward<T0>(x0), std::forward<T1>(x1)}} {}
+  template <std::ptrdiff_t I> decltype(auto) get() const {
     using std::get;
     return get<I>(data);
   }

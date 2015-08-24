1d315e9c7012cc64d4f7ae30f32c0228e5355f6f
Erik Schnetter <schnetter@gmail.com>
2015-08-24 13:21:36 -0400

    Reformat storage_t selection in wave1d

--- examples/wave1d.cc
+++ examples/wave1d.cc
@@ -167,11 +167,9 @@ constexpr std::size_t max_size = 16;
 template <typename T>
 using shared_maxarray = adt::nested<std::shared_ptr<adt::dummy>,
                                     adt::maxarray<adt::dummy, max_size>, T>;
-
 template <typename T>
 using future_maxarray = adt::nested<qthread::shared_future<adt::dummy>,
                                     adt::maxarray<adt::dummy, max_size>, T>;
-
 template <typename T>
 using proxy_maxarray = adt::nested<funhpc::proxy<adt::dummy>,
                                    adt::maxarray<adt::dummy, max_size>, T>;
@@ -180,20 +178,21 @@ template <typename T>
 using shared_maxarray_tree =
     adt::nested<adt::tree<shared_maxarray<adt::dummy>, adt::dummy>,
                 shared_maxarray<adt::dummy>, T>;
-
 template <typename T>
 using future_maxarray_tree =
     adt::nested<adt::tree<future_maxarray<adt::dummy>, adt::dummy>,
                 future_maxarray<adt::dummy>, T>;
-
 template <typename T>
 using proxy_maxarray_tree =
     adt::nested<adt::tree<proxy_maxarray<adt::dummy>, adt::dummy>,
                 proxy_maxarray<adt::dummy>, T>;
 
+// template <typename T> using storage_t = adt::maxarray<T, max_size>;
+
 // template <typename T> using storage_t = shared_maxarray<T>;
 // template <typename T> using storage_t = future_maxarray<T>;
 // template <typename T> using storage_t = proxy_maxarray<T>;
+
 // template <typename T> using storage_t = shared_maxarray_tree<T>;
 // template <typename T> using storage_t = future_maxarray_tree<T>;
 template <typename T> using storage_t = proxy_maxarray_tree<T>;

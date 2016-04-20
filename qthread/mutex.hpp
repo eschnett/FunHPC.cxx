9f2b1f2cebece2775f2eab20ac8375b74a293d27
Erik Schnetter <schnetter@gmail.com>
2016-04-20 11:52:55 -0400

    Implement unique_lock

--- qthread/mutex.hpp
+++ qthread/mutex.hpp
@@ -6,6 +6,8 @@
 #include <qthread/qt_syscalls.h>
 #include <qthread/qthread.hpp>
 
+#include <utility>
+
 namespace qthread {
 
 // mutex ///////////////////////////////////////////////////////////////////////
@@ -13,19 +15,83 @@ namespace qthread {
 class mutex {
   syncvar mem;
 
+  bool is_locked() { return !mem.status(); }
+
 public:
   mutex() noexcept {}
   mutex(const mutex &) = delete;
   mutex(mutex &&) = delete;
-  ~mutex() { cxx_assert(mem.status()); }
+  ~mutex() { cxx_assert(!is_locked()); }
   mutex &operator=(const mutex &) = delete;
   mutex &operator=(mutex &&) = delete;
   void lock() { mem.readFE(); }
+  // bool try_lock();
   void unlock() {
-    cxx_assert(!mem.status());
+    cxx_assert(is_locked());
     mem.fill();
   }
 };
+
+// unique_lock /////////////////////////////////////////////////////////////////
+
+template <typename M> class unique_lock {
+  M *mtx;
+  bool owned;
+
+public:
+  typedef M mutex_type;
+  unique_lock() noexcept : mtx(nullptr), owned(false) {}
+  unique_lock(const unique_lock &) = delete;
+  unique_lock(unique_lock &&other) noexcept : mtx(other.mtx),
+                                              owned(other.owned) {
+    other.mtx = nullptr;
+    other.owned = false;
+  }
+  explicit unique_lock(M &m) : mtx(&m), owned(false) { lock(); }
+  ~unique_lock() {
+    if (owned)
+      unlock();
+  }
+  unique_lock &operator=(const unique_lock &) = delete;
+  unique_lock &operator=(unique_lock &&other) {
+    if (owned)
+      unlock();
+    mtx = other.mtx;
+    owned = other.owned;
+    other.mtx = nullptr;
+    other.owned = false;
+    return *this;
+  }
+  M *mutex() const noexcept { return mtx; }
+  bool owns_lock() const noexcept { return owned; }
+  operator bool() const noexcept { return owned; }
+  void lock() {
+    cxx_assert(!owned);
+    mtx->lock();
+    owned = true;
+  }
+  // bool try_lock();
+  void unlock() {
+    cxx_assert(owned);
+    mtx->unlock();
+    owned = false;
+  }
+  void swap(unique_lock &other) noexcept {
+    using namespace std;
+    std::swap(mtx, other.mtx);
+    std::swap(owned, other.owned);
+  }
+  M *release() noexcept {
+    M *res = mtx;
+    mtx = nullptr;
+    owned = false;
+    return res;
+  }
+};
+template <typename M>
+void swap(unique_lock<M> &lhs, unique_lock<M> &rhs) noexcept {
+  lhs.swap(rhs);
+}
 
 // lock_guard //////////////////////////////////////////////////////////////////
 
@@ -33,12 +99,13 @@ template <typename M> class lock_guard {
   M &mtx;
 
 public:
+  typedef M mutex_type;
   explicit lock_guard(M &m) : mtx(m) { mtx.lock(); }
   lock_guard(const lock_guard &) = delete;
   lock_guard(lock_guard &&) = delete;
+  ~lock_guard() { mtx.unlock(); }
   lock_guard &operator=(const lock_guard &) = delete;
   lock_guard &operator=(lock_guard &&) = delete;
-  ~lock_guard() { mtx.unlock(); }
 };
 }
 
--- qthread/mutex_test.cc
+++ qthread/mutex_test.cc
@@ -13,6 +13,56 @@ TEST(qthreads_mutex, basic) {
   m.lock();
   m.unlock();
 
+  {
+    unique_lock<mutex> l0;
+    EXPECT_EQ(nullptr, l0.mutex());
+    EXPECT_FALSE(l0.owns_lock());
+    EXPECT_FALSE(bool(l0));
+    EXPECT_EQ(nullptr, l0.release());
+    unique_lock<mutex> l1(std::move(l0));
+    EXPECT_EQ(nullptr, l0.mutex());
+    EXPECT_EQ(nullptr, l1.mutex());
+    EXPECT_FALSE(l1.owns_lock());
+    EXPECT_FALSE(bool(l1));
+    EXPECT_EQ(nullptr, l1.release());
+
+    unique_lock<mutex> l(m);
+    EXPECT_EQ(&m, l.mutex());
+    EXPECT_TRUE(l.owns_lock());
+    EXPECT_TRUE(bool(l));
+    l.unlock();
+    EXPECT_EQ(&m, l.mutex());
+    EXPECT_FALSE(l.owns_lock());
+    EXPECT_FALSE(bool(l));
+    l.lock();
+    EXPECT_EQ(&m, l.mutex());
+    EXPECT_TRUE(l.owns_lock());
+    EXPECT_TRUE(bool(l));
+    auto m1 = l.release();
+    EXPECT_EQ(&m, m1);
+    EXPECT_EQ(nullptr, l.mutex());
+    m1->unlock();
+
+    {
+      unique_lock<mutex> ll(m);
+      EXPECT_TRUE(ll.owns_lock());
+      // destructing ll unlocks the mutex
+    }
+    m.lock();
+    m.unlock();
+
+    unique_lock<mutex> l2(m);
+    unique_lock<mutex> l3(std::move(l2));
+    EXPECT_EQ(nullptr, l2.mutex());
+    EXPECT_EQ(&m, l3.mutex());
+    swap(l2, l3);
+    EXPECT_EQ(&m, l2.mutex());
+    EXPECT_EQ(nullptr, l3.mutex());
+    l3 = std::move(l2);
+    EXPECT_EQ(nullptr, l2.mutex());
+    EXPECT_EQ(&m, l3.mutex());
+  }
+
   { lock_guard<mutex> g(m); }
 }
 

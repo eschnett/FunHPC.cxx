9a787d761b6cdf1f42242e9af5088e2b43be646c
Erik Schnetter <schnetter@gmail.com>
2017-02-22 22:03:58 -0500

    Correct configuration self-test

--- funhpc/config_test.cpp
+++ funhpc/config_test.cpp
@@ -1,9 +1,11 @@
 #include <funhpc/config.hpp>
 
+#include <string>
+
 #include <gtest/gtest.h>
 
 using namespace funhpc;
 
 TEST(funhpc_config, version) {
-  EXPECT_EQ(FUNHPC_VERSION, version());
+  EXPECT_EQ(std::string(FUNHPC_VERSION), std::string(version()));
 }

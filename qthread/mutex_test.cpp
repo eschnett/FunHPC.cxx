8c16c51d6d242a6606e5f388534dac9ca0e67003
Erik Schnetter <schnetter@gmail.com>
2017-08-19 23:09:24 -0400

    Make things compile with Clang

--- qthread/mutex_test.cpp
+++ qthread/mutex_test.cpp
@@ -11,24 +11,24 @@ using namespace qthread;
 using namespace std;
 
 TEST(qthreads_mutex, basic) {
-  mutex m;
+  qthread::mutex m;
   m.lock();
   m.unlock();
 
   {
-    unique_lock<mutex> l0;
+    qthread::unique_lock<qthread::mutex> l0;
     EXPECT_EQ(nullptr, l0.mutex());
     EXPECT_FALSE(l0.owns_lock());
     EXPECT_FALSE(bool(l0));
     EXPECT_EQ(nullptr, l0.release());
-    unique_lock<mutex> l1(std::move(l0));
+    qthread::unique_lock<qthread::mutex> l1(std::move(l0));
     EXPECT_EQ(nullptr, l0.mutex());
     EXPECT_EQ(nullptr, l1.mutex());
     EXPECT_FALSE(l1.owns_lock());
     EXPECT_FALSE(bool(l1));
     EXPECT_EQ(nullptr, l1.release());
 
-    unique_lock<mutex> l(m);
+    qthread::unique_lock<qthread::mutex> l(m);
     EXPECT_EQ(&m, l.mutex());
     EXPECT_TRUE(l.owns_lock());
     EXPECT_TRUE(bool(l));
@@ -46,15 +46,15 @@ TEST(qthreads_mutex, basic) {
     m1->unlock();
 
     {
-      unique_lock<mutex> ll(m);
+      qthread::unique_lock<qthread::mutex> ll(m);
       EXPECT_TRUE(ll.owns_lock());
       // destructing ll unlocks the mutex
     }
     m.lock();
     m.unlock();
 
-    unique_lock<mutex> l2(m);
-    unique_lock<mutex> l3(std::move(l2));
+    qthread::unique_lock<qthread::mutex> l2(m);
+    qthread::unique_lock<qthread::mutex> l3(std::move(l2));
     EXPECT_EQ(nullptr, l2.mutex());
     EXPECT_EQ(&m, l3.mutex());
     swap(l2, l3);
@@ -65,17 +65,17 @@ TEST(qthreads_mutex, basic) {
     EXPECT_EQ(&m, l3.mutex());
   }
 
-  { lock_guard<mutex> g(m); }
+  { qthread::lock_guard<qthread::mutex> g(m); }
 }
 
 TEST(qthreads_mutex, two_threads) {
   qthread_initialize();
 
-  mutex m;
+  qthread::mutex m;
   atomic<int> value{0};
   m.lock();
   thread t([&]() {
-    lock_guard<mutex> g(m);
+    qthread::lock_guard<qthread::mutex> g(m);
     value ^= 1;
   });
   this_thread::sleep_for(std::chrono::milliseconds(100));

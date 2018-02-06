fb9afdceab315b2f4747cfef12052fa54dbae069
Erik Schnetter <schnetter@gmail.com>
2018-02-05 23:08:34 -0500

    Support newer versions

--- package.py
+++ package.py
@@ -5,6 +5,9 @@ class Funhpc(CMakePackage):
     homepage = "https://github.com/eschnett/FunHPC.cxx"
     url= "https://github.com/eschnett/FunHPC.cxx/archive/version/0.1.0.tar.gz"
 
+    version('1.3.1', '935333f47790e2bc8558b43bd6a2033b')
+    version('1.3.0', '71a1e57c4d882cdf001f29122edf7fc6')
+    version('1.2.0', 'ba2bbeea3091e999b6b85eaeb1b67a83')
     version('1.1.1', '7b9ef638b02fffe35b75517e8eeff580')
     version('1.1.0', '897bd968c42cd4f14f86fcf67da70444')
     version('1.0.0', 'f34e71ccd5548b42672e692c913ba5ee')
